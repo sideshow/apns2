86c9dad4c67d302b4a54dd7cd29c73df8d0797aa
sideshow <adam@digirati.co.nz>
2020-01-08 22:33:44 +1300

    Fix webpush example payload

--- _example/token/token.go
+++ _example/token/token.go
@@ -38,8 +38,13 @@ func main() {
 	notification.DeviceToken = *deviceToken
 	notification.Topic = *topic
 	notification.Payload = []byte(`{
-			"aps" : {
-				"alert" : "Hello!"
+			"aps": {
+				"alert": {
+					"title": "Flight A998 Now Boarding",
+					"body": "Boarding has begun for Flight A998.",
+					"action": "View"
+				},
+				"url-args": ["boarding", "A998"]
 			}
 		}
 	`)
