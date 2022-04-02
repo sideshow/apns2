c0c7ebbbda2a931e562f3570e3f8426aed101cc8
Adam Jones <adam@digirati.co.nz>
2022-04-02 15:13:40 +1300

    Add location push type (#194)
    * Add location push type
    * Fix Typo
    * Add InvalidPushType reason error code

--- notification.go
+++ notification.go
@@ -11,41 +11,52 @@ type EPushType string
 const (
 	// PushTypeAlert is used for notifications that trigger a user interaction —
 	// for example, an alert, badge, or sound. If you set this push type, the
-	// apns-topic header field must use your app’s bundle ID as the topic. The
-	// alert push type is required on watchOS 6 and later. It is recommended on
-	// macOS, iOS, tvOS, and iPadOS.
+	// topic field must use your app’s bundle ID as the topic. If the
+	// notification requires immediate action from the user, set notification
+	// priority to 10; otherwise use 5. The alert push type is required on
+	// watchOS 6 and later. It is recommended on macOS, iOS, tvOS, and iPadOS.
 	PushTypeAlert EPushType = "alert"
 
 	// PushTypeBackground is used for notifications that deliver content in the
 	// background, and don’t trigger any user interactions. If you set this push
-	// type, the apns-topic header field must use your app’s bundle ID as the
-	// topic. The background push type is required on watchOS 6 and later. It is
-	// recommended on macOS, iOS, tvOS, and iPadOS.
+	// type, the topic field must use your app’s bundle ID as the topic. Always
+	// use priority 5. Using priority 10 is an error. The background push type
+	// is required on watchOS 6 and later. It is recommended on macOS, iOS,
+	// tvOS, and iPadOS.
 	PushTypeBackground EPushType = "background"
 
+	// PushTypeLocation is used for notifications that request a user’s
+	// location. If you set this push type, the topic field must use your app’s
+	// bundle ID with .location-query appended to the end. The location push
+	// type is recommended for iOS and iPadOS. It isn’t available on macOS,
+	// tvOS, and watchOS. If the location query requires an immediate response
+	// from the Location Push Service Extension, set notification apns-priority
+	// to 10; otherwise, use 5. The location push type supports only token-based
+	// authentication.
+	PushTypeLocation EPushType = "location"
+
 	// PushTypeVOIP is used for notifications that provide information about an
-	// incoming Voice-over-IP (VoIP) call. If you set this push type, the
-	// apns-topic header field must use your app’s bundle ID with .voip appended
-	// to the end. If you’re using certificate-based authentication, you must
-	// also register the certificate for VoIP services. The voip push type is
-	// not available on watchOS. It is recommended on macOS, iOS, tvOS, and
-	// iPadOS.
+	// incoming Voice-over-IP (VoIP) call. If you set this push type, the topic
+	// field must use your app’s bundle ID with .voip appended to the end. If
+	// you’re using certificate-based authentication, you must also register the
+	// certificate for VoIP services. The voip push type is not available on
+	// watchOS. It is recommended on macOS, iOS, tvOS, and iPadOS.
 	PushTypeVOIP EPushType = "voip"
 
 	// PushTypeComplication is used for notifications that contain update
 	// information for a watchOS app’s complications. If you set this push type,
-	// the apns-topic header field must use your app’s bundle ID with
-	// .complication appended to the end. If you’re using certificate-based
-	// authentication, you must also register the certificate for WatchKit
-	// services. The complication push type is recommended for watchOS and iOS.
-	// It is not available on macOS, tvOS, and iPadOS.
+	// the topic field must use your app’s bundle ID with .complication appended
+	// to the end. If you’re using certificate-based authentication, you must
+	// also register the certificate for WatchKit services. The complication
+	// push type is recommended for watchOS and iOS. It is not available on
+	// macOS, tvOS, and iPadOS.
 	PushTypeComplication EPushType = "complication"
 
 	// PushTypeFileProvider is used to signal changes to a File Provider
-	// extension. If you set this push type, the apns-topic header field must
-	// use your app’s bundle ID with .pushkit.fileprovider appended to the end.
-	// The fileprovider push type is not available on watchOS. It is recommended
-	// on macOS, iOS, tvOS, and iPadOS.
+	// extension. If you set this push type, the topic field must use your app’s
+	// bundle ID with .pushkit.fileprovider appended to the end. The
+	// fileprovider push type is not available on watchOS. It is recommended on
+	// macOS, iOS, tvOS, and iPadOS.
 	PushTypeFileProvider EPushType = "fileprovider"
 
 	// PushTypeMDM is used for notifications that tell managed devices to
